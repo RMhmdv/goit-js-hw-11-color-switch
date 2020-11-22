ce5c4a14202da1a5e56a1af5b24b34aae4021465
Ravshan Mahmudov <2321772@gmail.com>
2020-11-23 00:14:22 +0200

    добавляет файлы проекта

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './styles.css';
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"'),
+    stopBtn: document.querySelector('button[data-action="stop"'),
+}
+  
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+  const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+ const setRandomColor = () => {
+  const currentColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  console.log(currentColor);
+  document.body.style.backgroundColor = currentColor;
+}
+
+    let interval = undefined;
+
+refs.startBtn.addEventListener('click',onStartBtnClick );
+
+
+function onStartBtnClick() {
+  
+  return interval = interval ? interval : setInterval(() => setRandomColor(), 1000);
+
+}
+
+
+refs.stopBtn.addEventListener('click', () => clearInterval(interval));
